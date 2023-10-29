8c1c773dc8ad25c376b5beb6e9e1b94c925af6a7
grubor-maja <maja.grubor@outlook.com>
2023-10-29 22:04:07 +0100

    Footer defined

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     var navbar = document.querySelector('.nav-bar__list');
     navbar.classList.toggle('mobile-active')
 }
+
+const collapsibles = document.querySelectorAll(".collapsible");
+
+if(window.innerWidth<=768) {
+collapsibles.forEach((item) =>{
+    item.addEventListener("click", function() {
+        this.classList.toggle("collapsible--expanded");
+    })
+})
+}
